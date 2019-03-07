8c8018cc318641b364beb8bb95587211e71971b1
Timothy Seah <timothyseah.t.e@gmail.com>
2019-03-07 17:11:20 +0800

    Display relatively ok formatted pictures from unsplash

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
 import "./App.css";
 import Dictaphone from "./Components/Dictaphone/Dictaphone";
+// import Background from "./img/bg/lensgrid.png";
+import Background from "./Components/Background/Background";
+import { searchPhotos } from "./services/unsplash";
 
 class App extends Component {
   state = {
     searchTerms: {},
-    command: ""
+    command: "",
+    resultList: [],
+
+    style: {
+      height: "100vh",
+      width: "100%"
+    }
   };
+
+  async componentDidMount() {
+    try {
+      const searchTerm = "dog";
+      const response = await searchPhotos(searchTerm);
+      console.log(response.results);
+      const results = response.results;
+      const oneResult = response.results[2].urls.regular;
+      this.setState({ resultList: results });
+
+      const styleCopy = { ...this.state.style };
+      console.log(styleCopy);
+      console.log(oneResult);
+
+      this.setState({ newBackground: oneResult });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   getSearchTerms = words => {
     this.setState({ searchTerms: words });
@@ -16,24 +44,36 @@ class App extends Component {
     this.setState({ command: words });
   };
 
+  getBackground = style => {
+    this.setState({ style: style });
+  };
+
   render() {
+    const imgUrl = this.state.newBackground;
+    const divStyle = {
+      backgroundImage: "url(" + imgUrl + ")"
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <p>What are your commands?</p>
-          <img
-            src={require("./img/samaritan-signal.png")}
-            className="App-logo"
-            alt="logo"
-          />
-          <div>{this.state.command}</div>
-          <div>
-            <Dictaphone
-              sendSearchTerms={this.getSearchTerms}
-              sendCommand={this.getCommand}
+        <div className="AppTest" style={divStyle}>
+          <header className="App-header">
+            <p>What are your commands?</p>
+            <img
+              src={require("./img/samaritan-signal.png")}
+              className="App-logo"
+              alt="logo"
             />
-          </div>
-        </header>
+            <div>{this.state.command}</div>
+            <div>
+              <Dictaphone
+                sendSearchTerms={this.getSearchTerms}
+                sendCommand={this.getCommand}
+              />
+              {/* <Background imageUrl={this.state.newBackground} /> */}
+            </div>
+          </header>
+        </div>
       </div>
     );
   }
--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-function Background(searchTerm) {
-  return <div />;
+function Background({ imageUrl }) {
+  const styles = { width: "18rem" };
+
+  return (
+    <div
+      className="card m-3"
+      style={{
+        top: "0",
+        bottom: "0",
+        left: "0",
+        right: "0",
+        position: "absolute"
+      }}
+    >
+      <img src={imageUrl} className="card-img-top" alt="..." />
+      <div className="card-body" />
+    </div>
+  );
 }
 
 export default Background;
